4375582f4ff5d7a8e07268c82552e280d396543a
Maderaj <145520236+Maderaj@users.noreply.github.com>
2023-09-20 22:45:35 +0800

    Update

--- cc206_exercise_unit3/lib/main.dart
+++ cc206_exercise_unit3/lib/main.dart
@@ -1,5 +1,5 @@
 //{Simon Troy Eligido}: {Dont problem the problem, let the problem, problem you}
-//{Ailen Grace Malcon}: {Kun nd kaya,veriton}
+//{Ailen Grace Malcon}: {Kun nd kaya,veriton.}
 //{Jannah Madera}: {So near and yet so far.}
 
 import 'package:flutter/material.dart';
